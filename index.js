5f2efce76cb00cd4e86df5faf8b8eac60e06e764
FrranzMaurer <38557647+FrranzMaurer@users.noreply.github.com>
2018-08-28 19:26:51 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ var server = http.createServer();
 server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && request.url === '/') {
-        fs.readFile('./index.js', 'utf-8', function(err, data) {
-        });
-        response.write(data);
+            fs.readFile('./index.js', 'utf-8', function(data) {
+            response.write(data);
             response.end();
+        });
     } else {
             response.statusCode = 404;
-            response.write('<h1>404: Zła ścieżka!</h1>');
+            response.write("<img src=" + 'https://discourse-cdn-sjc1.com/business5/uploads/gemsofwar/original/3X/7/e/7e9b5761595dcfeda805afa3d77a7ba5e81b06c1.jpg' + "/>");
             response.end();
     }
 });
-
 server.listen(8080);
